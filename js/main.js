a39547429d03b123e6a27c69fed9bd250187e909
cyrilmuchemi <cyrilmuchemi@gmail.com>
2024-06-18 19:04:05 +0300

    Add a floor plane

--- js/main.js
+++ js/main.js
@@ -31,8 +31,22 @@ scene.add(sunLight);
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({color: 0xff000});
 let cube = new THREE.Mesh(geometry, material);
-
 scene.add(cube);
+
+//floor texture
+let textureLoader = new THREE.TextureLoader();
+let floorTexture = textureLoader.load("img/Floor.jpg");
+
+
+//Create the floor plane
+const planeGeometry = new THREE.PlaneGeometry(50, 50, 100, 100);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    map: floorTexture,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+
 
 //Controlls
 document.addEventListener('keydown', onkeydown, false);
